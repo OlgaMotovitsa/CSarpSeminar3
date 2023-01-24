ac62a4593a579207b5ad7e1a64d7a97b858120ef
DFIT Ольга <Olga.Motovitsa@gmail.com>
2023-01-24 17:14:19 +0300

    add gitignore

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+int number1 = new Random().Next(1,5);
+Console.WriteLine(number1);
+if (number1 == 1) 
+{
+    Console.WriteLine("x:= (0; +бесконечность)     y:=(0; +бесконечность)");
+}
+else if (number1 == 2) 
+{
+    Console.WriteLine("x:= (0; -бесконечность)     y:=(0; +бесконечность)");
+}
+else if (number1 == 3) 
+{
+    Console.WriteLine("x:= (0; -бесконечность)     y:=(0; -бесконечность)");
+}
+else if (number1 == 4) 
+{
+    Console.WriteLine("x:= (0; +бесконечность)     y:=(0; -бесконечность)");
+}
+else if ((number1 <= 0) || (number1 >= 4))
+{
+    Console.WriteLine("Такой четверти нет");
+}
