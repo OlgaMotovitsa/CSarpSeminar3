150fbf3c63a08653d40f4c7323c7e8da5f916d97
DFIT Ольга <Olga.Motovitsa@gmail.com>
2023-01-24 18:11:11 +0300

    add HomeWork

---
+++ HW2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход координаты 2х точек 
+// и находит расстояние между ними в 3Д пространстве.
+
+// точка А (3,6,8)
+// точка B (2,1,-7)
+// ответ = 15.84
+
+Console.Clear();
+
+Random rnd = new Random();
+int x1 = rnd.Next(1,11);
+int y1 = rnd.Next(1,11);
+int z1 = rnd.Next(1,11);
+int x2 = rnd.Next(1,11);
+int y2 = rnd.Next(1,11);
+int z2 = rnd.Next(1,11);
+
+System.Console.WriteLine($"x1 = {x1}, y1 = {y1}, z1 = {z1}, x2 = {x2}, y2 = {y2}, z2 = {z2}");
+
+double result = Math.Sqrt(Math.Pow((x1-x2), 2) + Math.Pow((y1-y2), 2) + Math.Pow((z1-z2), 2));
+
+System.Console.WriteLine(result);
---
+++ HW3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+
+int num = new Random().Next(1,10);
+System.Console.WriteLine($"num = {num}");
+
+for(int i = 1; i <= num; i ++)
+{
+    System.Console.WriteLine(Math.Pow(i, 3));
+}
